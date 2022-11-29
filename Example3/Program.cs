5ad832d66f5b0a221d1f651e0a8930ea5eaff8f2
Ellina Korzhova <ellina.korzhpva@mail.ru>
2022-11-29 20:22:25 +0300

    Домашнее задание 2

---
+++ Example3/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет*/
+
+
+Console.WriteLine("Введите день недели");
+string numberOfTheDayStr = Console.ReadLine();
+int numberOfTheDay = int.Parse(numberOfTheDayStr);
+
+switch (numberOfTheDay) {
+    case 1:
+        Console.WriteLine("Это не выходной");
+        break;
+    case 2:
+        Console.WriteLine("Это не выходной");
+        break;
+    case 3:
+        Console.WriteLine("Это не выходной");
+        break;
+    case 4:
+        Console.WriteLine("Это не выходной");
+        break;
+    case 5:
+        Console.WriteLine("Это не выходной");
+        break;  
+    case 6:
+        Console.WriteLine("Это выходной");
+        break;
+     case 7:
+        Console.WriteLine("Это выходной");
+        break;
+    default:
+        Console.WriteLine("Введите число от 1 до 7");
+        break;
+}
